b9602fb372dd896149de93889ee81e64016d952c
Sylvain Dethier <dethiersylvain@gmail.com>
2024-05-11 14:20:59 +0200

    Update String.d.ts

--- String.d.ts
+++ String.d.ts
@@ -1,49 +1,69 @@
 /**
- * Each Type function has for least types:
+ * Each type test function has for least types arguments:
  * @typeParam Yes - The type to return in case the test is positive
  * @typeParam No - The type to return  in case the test is negative
  */
 
 /**
- * Test a string that represents a number
- * @typeParam S - The string to test
+ * Test a string to be generic `string`
+ * @typeParam StringType - The string to test
  */
-export type NumberString<S extends string, Yes = S, No = never> = S extends `${number}` ? Yes : No;
+export type IfGenericString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = StringType extends `${any}` ? No : Yes;
+
+/**
+ * Test a string that represents a number
+ * @typeParam StringType - The string to test
+ */
+export type IfNumberString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${number}` ? Yes : No;
 
 /**
  * Test a string that represents a bigint
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type BigIntString<S extends string, Yes = S, No = never> = S extends `${number}n` ? Yes : No;
+export type IfBigIntString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${number}n` ? Yes : No;
 
 /**
  * Test a string that represents a boolean
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type BooleanString<S extends string, Yes = S, No = never> = S extends `${boolean}`
-  ? Yes
-  : No;
+export type IfBooleanString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${boolean}` ? Yes : No;
 
 /**
  * Test a string that represents null
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type NullString<S extends string, Yes = S, No = never> = S extends `${null}` ? Yes : No;
+export type IfNullString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${null}` ? Yes : No;
 
 /**
  * Test a string that represents undefined
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type UndefinedString<S extends string, Yes = S, No = never> = S extends `${undefined}`
-  ? Yes
-  : No;
-
-/**
- * Test a string to not be generic `string`
- * @typeParam S - The string to test
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type NotString<S extends string, Yes = S, No = never> = S extends `${any}` ? Yes : No;
+export type IfUndefinedString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${undefined}` ? Yes : No;
 
 /**
  * Represents any whitespace character
@@ -52,47 +72,55 @@ export type Whitespace = " " | "\t" | "\r" | "\n" | "\v" | "\f";
 
 /**
  * Test a string that represents undefined
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type WhitespaceString<S extends string, Yes = S, No = never> = S extends `${Whitespace}`
-  ? Yes
-  : No;
+export type IfWhitespaceString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends `${Whitespace}` ? Yes : No;
 
 /**
  * Test a string to be empty
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type EmptyString<S extends string, Yes = S, No = never> = S extends "" ? Yes : No;
+export type IfEmptyString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = StringType extends "" ? Yes : No;
 
 /**
  * Test a string to be not empty
- * @typeParam S - The string to test
+ * @typeParam StringType - The string to test
  */
-export type NotEmptyString<S extends string, Yes = S, No = never> = EmptyString<S> extends never
-  ? Yes
-  : No;
+export type IfNotEmptyString<
+  StringType extends string,
+  Yes = StringType,
+  No = never
+> = IfEmptyString<StringType> extends never ? Yes : No;
 
 /**
  * Trim a string from its end
  * Removes whitespace characters from the end of a string
- * @typeParam S - The string to trim from end
+ * @typeParam StringType - The string to trim from end
  */
-export type TrimEnd<S extends string> = S extends `${infer substr}${Whitespace}`
+export type TrimEnd<StringType extends string> = StringType extends `${infer substr}${Whitespace}`
   ? `${TrimEnd<substr>}`
-  : S;
+  : StringType;
 
 /**
  * Trim a string from its start
  * Removes whitespace characters from the start of a string
- * @typeParam S - The string to trim from start
+ * @typeParam StringType - The string to trim from start
  */
-export type TrimStart<S extends string> = S extends `${Whitespace}${infer substr}`
+export type TrimStart<StringType extends string> = StringType extends `${Whitespace}${infer substr}`
   ? `${TrimStart<substr>}`
-  : S;
+  : StringType;
 
 /**
  * Trim a string
  * Removes whitespace characters from the start and the end of a string
- * @typeParam S - The string to trim
+ * @typeParam StringType - The string to trim
  */
-export type Trim<S extends string> = TrimStart<TrimEnd<S>>;
+export type Trim<StringType extends string> = TrimStart<TrimEnd<StringType>>;
